b2665913d71c218eac85ad4e2ac0fe87986d0890
BrissAcevedo <22300036@uttt.edu.mx>
2025-12-04 14:24:31 -0600

    remove node_modules5

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,10 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: './', // necesario para que Capacitor lo cargue bien
+  base: './',
   plugins: [vue()],
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://www.loginceci.somee.com/api/Users', // 🔹 URL hosteada en Somee
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ''), // ajusta la ruta para que axios use /login, /register, etc.
-      },
-    },
-  },
-});
+  build: {
+    outDir: 'dist'
+  }
+})
